b015065e3382de8d7512da916e07a01ba192f8e2
kirrishima <maxfunmaxfun672@gmail.com>
2022-11-29 20:04:37 +0300

    Telegram Bot

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from telebot import types
 import Articles
 from random import sample
 
-
 bot = telebot.TeleBot("ТОКЕН ВСТАВИТЬ СЮДА ВМЕСТО ЭТОГОГО ТЕКСТА БЕЗ ПРОБЕЛОВ", # можешь заменить этот токен внутри ковычек на свой либо оставить и использовать моего бота
                       parse_mode=None)
 
